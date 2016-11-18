ea995a0935b31ffed77478c7c2138146a6123222
Andrei Stalbe <andreistalbe@gmail.com>
2016-11-18 23:42:54 +0200

    Fixed lofs

--- test/api/loggly.js
+++ test/api/loggly.js
@@ -6,87 +6,96 @@ const Levels = require('../../lib/levels')
 
 let CONFIG = {
   transports: {
-      console: {
-        level: Levels.toArray(),
-        options: {
-          silent: false
-        }
-      },
       loggly: {
           level: Levels.toArray(),
           options: {
               token: 'c627e174-89ef-4743-a949-28616c69c682',
-              domain: 'c8management',
-              env: 'INT',
-              productName: 'logger',
-              serviceName: 'mocha'
+              subdomain: 'c8management',
+              tags: ['mocha', 'logger'],
+              json: true,
+              stripColors: true
           }
       }
   }
 }
 
-describe('Test Console Logger', function () {
+describe.only('Test Loggly Logger', function () {
   before(function () {
     Logger.init(CONFIG)
   })
 
-  it('- test debug method', function () {
-    Logger.debug('debug')
+  it('- test debug method', function (done) {
+    Logger.debug('DEBUG')
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test info method', function () {
+  it('- test info method', function (done) {
     Logger.info('info')
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test notice method', function () {
+  it('- test notice method', function (done) {
     Logger.notice('notice')
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test warning method', function () {
+  it('- test warning method', function (done) {
     Logger.warning('warning')
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test error method', function () {
+  it('- test error method', function (done) {
     Logger.error('error')
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test crit method', function () {
+  it('- test crit method', function (done) {
     Logger.crit('crit')
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test alert method', function () {
+  it('- test alert method', function (done) {
     Logger.alert('alert')
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test emerg method', function () {
+  it('- test emerg method', function (done) {
     Logger.emerg('emerg')
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test emerg method', function () {
+  it('- test emerg method', function (done) {
     Logger.log('emerg', 'log.emerg')
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test EvalError', () => {
+  it('- test EvalError', (done) => {
     Logger.error('Eval Error Test', new EvalError('My Eval Error'))
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test SyntaxError', () => {
+  it('- test SyntaxError', (done) => {
     Logger.error('Syntax Error Test', new SyntaxError('My Syntax Error'))
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test RangeError', () => {
+  it('- test RangeError', (done) => {
     Logger.error('Range Error Test', new RangeError('My Range Error'))
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test ReferenceError', () => {
+  it('- test ReferenceError', (done) => {
     Logger.error('Reference Error Test', new ReferenceError('My Reference Error'))
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test TypeError', () => {
+  it('- test TypeError', (done) => {
     Logger.error('Type Error Test', new TypeError('My Type Error'))
+    setTimeout(() => done(), 1000)
   })
 
-  it('- test URIError', () => {
+  it('- test URIError', (done) => {
     Logger.error('URI Error Test', new URIError('My URI Error'))
+    setTimeout(() => done(), 1000)
   })
 })
